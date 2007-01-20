4901f0644be7d2e35fcb772b65f66cb26f6a29d9
jferris <jferris@dc715f54-e70d-0410-b702-da5329ab684b>
2007-01-20 07:50:39 +0000

    Renamed from quick_forms to labelled_form
    git-svn-id: http://svn.optimistictech.com/ruby/rails/plugins/quick_forms@329 dc715f54-e70d-0410-b702-da5329ab684b

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-require 'quick_forms'
+require 'labelled_form'
 ActionView::Base.class_eval do
 	include ActionView::Helpers::LabelHelper
 	include ActionView::Helpers::LabelledFormHelper
---
+++ lib/labelled_form.rb
@@ -0,0 +1,3 @@
+require 'labelled_form/label_helper'
+require 'labelled_form/labelled_form_helper'
+require 'labelled_form/form_section_helper'
