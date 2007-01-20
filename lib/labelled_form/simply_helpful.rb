e4a80c03bf569993d6441e6e05b2db1bdb71c8f2
jferris <jferris@dc715f54-e70d-0410-b702-da5329ab684b>
2007-01-20 10:19:48 +0000

    Fixed simply_helpful integration to only activate when simply_helpful is available
    git-svn-id: http://svn.optimistictech.com/ruby/rails/plugins/labelled_form@337 dc715f54-e70d-0410-b702-da5329ab684b

--- lib/labelled_form/simply_helpful.rb
+++ lib/labelled_form/simply_helpful.rb
@@ -3,7 +3,11 @@ module ActionView #:nodoc:
 		module LabelledFormHelper #:nodoc:
 		
 			def labelled_form_for_with_record_identification(name_or_object, *args, &proc)
-				form_method_with_record_identification :labelled_form_for, name_or_object, *args, &proc
+				if self.respond_to?(:form_method_with_record_identification)
+					form_method_with_record_identification :labelled_form_for, name_or_object, *args, &proc
+				else
+					labelled_form_for_without_record_identification(name_or_object, *args, &proc)
+				end
 			end
 			
 			alias_method_chain :labelled_form_for, :record_identification
