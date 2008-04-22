f67e63dabc3f7ecc7c8d32a468ead37cf36075c8
Joe Ferris <jferris@Shiny.local>
2008-04-22 19:56:02 -0400

    Converted the form section helper tests to rspec

--- test/labelled_form/test_form_section_helper.rb
+++ test/labelled_form/test_form_section_helper.rb
@@ -1,159 +1,195 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-class FormSectionHelperTest < Test::Unit::TestCase
+describe "form section helpers" do
 	
-	def test_form_section_builder_module
-		template = <<-end_template
-			<%
-				s = Object.new
-				class << s
-					include Helpers::FormSectionBuilder
-				end
-				s.section_title = 'Section Title'
-			%>
-			<% s.info do %>
-				<p>I am the info</p>
-			<% end %>
-			<% s.body do %>
-				<p>I am the body</p>
-			<% end %>
-			<%= s.output %>
-		end_template
-		render(template)
-		
-		assert_tag({
-			:tag => 'div',
-			:attributes => {:class => 'section_info'},
-			:child => {
-				:tag => 'h2',
-				:content => 'Section Title'
-			}
-		})
-		assert_tag({
-			:tag => 'div',
-			:attributes => {:class => 'section_info'},
-			:child => {
-				:tag => 'p',
-				:content => 'I am the info'
-			}
-		})
-		assert_tag({
-			:tag => 'div',
-			:attributes => {:class => 'section_body'},
-			:child => {
-				:tag => 'p',
-				:content => 'I am the body'
-			}
-		})
-	end
-	
-	def test_form_section
-		template = <<-end_template
-			<% form_section(:title => 'Section Title') do |s| %>
-				<% s.info do %>
-					<p>I am the info</p>
-				<% end %>
-				<% s.body do %>
-					<p>I am the body</p>
-				<% end %>
-			<% end %>
-		end_template
-		render(template)
-		
-		assert_tag({
-			:tag => 'div',
-			:attributes => {:class => 'section'},
-			:child => {:tag => 'div'}
-		})
-	end
-	
-	def test_form_section_for
-		template = <<-end_template
-			<% form_section_for(:var, @var, :title => 'Section Title') do |s| %>
-				<% s.info do %>
-					<p>I am the info</p>
-				<% end %>
-				<% s.body do %>
-					<%= s.text_field :name %>
-				<% end %>
-			<% end %>
-		end_template
-		render(template, :name => 'test')
-		
-		assert_tag({
-			:tag => 'div',
-			:attributes => {:class => 'section_body'},
-			:child => {
-				:tag => 'input',
-				:attributes => {
-					:id => 'var_name',
-					:value => 'test'
-				}
-			}
-		})
-	end
-	
-	def test_form_builder_section
-		template = <<-end_template
-			<% form_for(:var, @var) do |f| %>
-				<% f.section :title => 'Section Title' do |s| %>
-					<% s.info do %>
-						<p>I am the info</p>
-					<% end %>
-					<% s.body do %>
-						<%= s.text_field :name %>
-					<% end %>
-				<% end %>
-			<% end %>
-		end_template
-		render(template, :name => 'test')
-		
-		assert_tag({
-			:tag => 'form',
-			:child => {
-				:tag => 'div',
-				:attributes => {:class => 'section'},
-				:child => {
-					:tag => 'div',
-					:attributes => {:class => 'section_body'},
-					:child => {
-						:tag => 'input',
-						:attributes => {
-							:id => 'var_name',
-							:value => 'test'
-						}
-					}
-				}
-			}
-		})
-	end
-	
-	def test_labelled_form_builder_section
-		template = <<-end_template
-			<% labelled_form_for(:var, @var) do |f| %>
-				<% f.section :title => 'Section Title' do |s| %>
-					<% s.info do %>
-						<p>I am the info</p>
-					<% end %>
-					<% s.body do %>
-						<%= s.text_field :name %>
-					<% end %>
-				<% end %>
-			<% end %>
-		end_template
-		render(template, :name => 'test')
+  context "building a form section" do
 
-		assert_tag({
-			:tag => 'form',
-			:attributes => {:class => 'labelled'},
-			:descendant => {
-				:tag => 'div',
-				:attributes => {
-					:class => 'value_field field',
-					:id => 'var_name_field'
-				}
-			}
-		})
+    before do
+      template = <<-end_template
+        <%
+          s = Object.new
+          class << s
+            include Helpers::FormSectionBuilder
+          end
+          s.section_title = 'Section Title'
+        %>
+        <% s.info do %>
+          <p>I am the info</p>
+        <% end %>
+        <% s.body do %>
+          <p>I am the body</p>
+        <% end %>
+        <%= s.output %>
+      end_template
+      render(template)
+    end
+		
+    it "should create a title section" do
+      assert_tag({
+        :tag => 'div',
+        :attributes => {:class => 'section_info'},
+        :child => {
+          :tag => 'h2',
+          :content => 'Section Title'
+        }
+      })
+    end
+
+    it "should create an info section" do
+      assert_tag({
+        :tag => 'div',
+        :attributes => {:class => 'section_info'},
+        :child => {
+          :tag => 'p',
+          :content => 'I am the info'
+        }
+      })
+    end
+
+    it "should create a body section"do
+      assert_tag({
+        :tag => 'div',
+        :attributes => {:class => 'section_body'},
+        :child => {
+          :tag => 'p',
+          :content => 'I am the body'
+        }
+      })
+    end
+
+	end
+	
+  context "creating a form section" do
+
+    before do
+      template = <<-end_template
+        <% form_section(:title => 'Section Title') do |s| %>
+          <% s.info do %>
+            <p>I am the info</p>
+          <% end %>
+          <% s.body do %>
+            <p>I am the body</p>
+          <% end %>
+        <% end %>
+      end_template
+      render(template)
+    end
+      
+    it "should create a div for the section" do
+      assert_tag({
+        :tag => 'div',
+        :attributes => {:class => 'section'},
+        :child => {:tag => 'div'}
+      })
+    end
+
+  end
+	
+  context "create a form section for an object" do
+
+    before do
+      template = <<-end_template
+        <% form_section_for(:var, @var, :title => 'Section Title') do |s| %>
+          <% s.info do %>
+            <p>I am the info</p>
+          <% end %>
+          <% s.body do %>
+            <%= s.text_field :name %>
+          <% end %>
+        <% end %>
+      end_template
+      render(template, :name => 'test')
+    end
+		
+    it "should create the section body" do
+      assert_tag({
+        :tag => 'div',
+        :attributes => {:class => 'section_body'},
+        :child => {
+          :tag => 'input',
+          :attributes => {
+            :id => 'var_name',
+            :value => 'test'
+          }
+        }
+      })
+    end
+
+	end
+	
+  context "building a form section from a builder" do
+
+    setup do
+      template = <<-end_template
+        <% form_for(:var, @var) do |f| %>
+          <% f.section :title => 'Section Title' do |s| %>
+            <% s.info do %>
+              <p>I am the info</p>
+            <% end %>
+            <% s.body do %>
+              <%= s.text_field :name %>
+            <% end %>
+          <% end %>
+        <% end %>
+      end_template
+      render(template, :name => 'test')
+    end
+		
+    it "should create the form tag" do
+      assert_tag({
+        :tag => 'form',
+        :child => {
+          :tag => 'div',
+          :attributes => {:class => 'section'},
+          :child => {
+            :tag => 'div',
+            :attributes => {:class => 'section_body'},
+            :child => {
+              :tag => 'input',
+              :attributes => {
+                :id => 'var_name',
+                :value => 'test'
+              }
+            }
+          }
+        }
+      })
+    end
+
+	end
+	
+  context "building a section from a labelled from builder" do
+
+    before do
+      template = <<-end_template
+        <% labelled_form_for(:var, @var) do |f| %>
+          <% f.section :title => 'Section Title' do |s| %>
+            <% s.info do %>
+              <p>I am the info</p>
+            <% end %>
+            <% s.body do %>
+              <%= s.text_field :name %>
+            <% end %>
+          <% end %>
+        <% end %>
+      end_template
+      render(template, :name => 'test')
+    end
+
+    it "should build a labelled form" do
+      assert_tag({
+        :tag => 'form',
+        :attributes => {:class => 'labelled'},
+        :descendant => {
+          :tag => 'div',
+          :attributes => {
+            :class => 'value_field field',
+            :id => 'var_name_field'
+          }
+        }
+      })
+    end
+
 	end
 	
 end
