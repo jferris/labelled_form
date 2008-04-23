6d85130d4213fffaacaabfa22ab2222f85d47b67
Joe Ferris <jferris@Shiny.local>
2008-04-22 20:08:00 -0400

    Converted the label helper tests to rspec

--- test/labelled_form/test_label_helper.rb
+++ test/labelled_form/test_label_helper.rb
@@ -1,46 +1,98 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-class LabelHelperTest < Test::Unit::TestCase
+describe "label helpers" do
 	
-	def test_label_tag
-		render(%{<%= label_tag(:address) %>})
-		assert_tag :tag => 'label',
-		:attributes => {:for => 'address'},
-		:content => 'Address:'
+  context "label_tag" do
+
+    context "with only a symbol" do
+
+      before do
+        render(%{<%= label_tag(:address) %>})
+      end
+
+      it "should generate a label tag using the humanized symbol as the label" do
+        assert_tag(:tag => 'label',
+                   :attributes => {:for => 'address'},
+                   :content => 'Address:')
+      end
+
+    end
 		
-		result = render(%{<%= label_tag(:address, "Mailing Address:") %>})
-		assert_tag :tag => 'label',
-		:attributes => {:for => 'address'},
-		:content => 'Mailing Address:'
+    context "with a symbol and a string" do
+
+      before do
+        result = render(%{<%= label_tag(:address, "Mailing Address:") %>})
+      end
+
+      it "should generate a label tag using the string as a label" do
+        assert_tag :tag => 'label',
+        :attributes => {:for => 'address'},
+        :content => 'Mailing Address:'
+      end
 		
-		result = render(%{<%= label_tag(nil, "Mailing Address:") %>})
-		assert_tag :tag => 'label',
-		:attributes => {:for => nil},
-		:content => 'Mailing Address:'
+    end
+
+    context "with nil and a string" do
+
+      before do
+        result = render(%{<%= label_tag(nil, "Mailing Address:") %>})
+      end
+
+      it "should generate a label tag using the string as a label" do
+        assert_tag :tag => 'label',
+        :attributes => {:for => nil},
+        :content => 'Mailing Address:'
+      end
 		
-		result = render(%{<%= label_tag(:address, "Mailing Address:", :class => 'test') %>})
-		assert_tag :tag => 'label',
-		:attributes => {:for => 'address', :class => 'test'},
-		:content => 'Mailing Address:'
+    end
+
+    context "with a symbol, a string, and an options hash" do
+
+      before do
+        result = render(%{<%= label_tag(:address, "Mailing Address:", :class => 'test') %>})
+      end
+      
+      it "should generate a label tag and append options for the tag" do
+        assert_tag :tag => 'label',
+        :attributes => {:for => 'address', :class => 'test'},
+        :content => 'Mailing Address:'
+      end
+
+    end
+
 	end
 	
-	def test_label
-		render(%{<%= label(:var, :name) %>}, :name => 'test')
-		assert_tag :tag => 'label',
-		:attributes => {:for => 'var_name'},
-		:content => 'Name:'
-	end
+  context "generating a label for an attribute" do
+
+    before do
+      render(%{<%= label(:var, :name) %>}, :name => 'test')
+    end
+
+    it "should use the humanized attribute name as the label" do
+      assert_tag :tag => 'label',
+      :attributes => {:for => 'var_name'},
+      :content => 'Name:'
+    end
+
+  end
 	
-	def test_label_builder
-		template = <<EOF
-<% form_for(:var, @var) do |f| %>
-<%= f.label(:name) %>
-<% end %>
-EOF
-		render(template, :name => 'test')
-		assert_tag :tag => 'label',
-		:attributes => {:for => 'var_name'},
-		:content => 'Name:'
+  context "generating a label from a form builder" do
+
+    before do
+      template = <<-EOF
+        <% form_for(:var, @var) do |f| %>
+        <%= f.label(:name) %>
+        <% end %>
+      EOF
+      render(template, :name => 'test')
+    end
+
+    it "should generate a label tag using the form builder's object" do
+      assert_tag :tag => 'label',
+      :attributes => {:for => 'var_name'},
+      :content => 'Name:'
+    end
+
 	end
-	
+
 end
