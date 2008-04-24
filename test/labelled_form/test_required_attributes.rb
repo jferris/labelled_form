ff9b4a802f61acaaa5bc4e49781bcfd29b8223f3
Joe Ferris <jferris@Shiny.local>
2008-04-24 18:56:56 -0400

    Converted test_require_attributes to use matchers instead of assertions

--- test/labelled_form/test_required_attributes.rb
+++ test/labelled_form/test_required_attributes.rb
@@ -3,22 +3,18 @@ require File.dirname(__FILE__) + '/../test_helper'
 context "required attributes" do
 	
   it "should return the correct attributes for required_attributes_by_on" do
-		expected = {
-			:save => [:name]
-		}
-		assert_equal expected, Person.required_attributes_by_on, "The correct required attributes should be returned"
+		Person.required_attributes_by_on.should == { :save => [:name] }
 	end
 	
   it "should return an array of required attributes" do
-		person = Person.new
-		assert_equal [:name], person.required_attributes
+		Person.new.required_attributes.should == [:name]
 	end
 	
   it "should return true if an attribute is required" do
 		person = Person.new
 		
-		assert person.attribute_required?(:name), "The name attribute should be required"
-		assert !person.attribute_required?(:address), "The address attribute should not be required"
+		person.attribute_required?(:name).should == true
+		!person.attribute_required?(:address).should == true
 	end
 	
 end
