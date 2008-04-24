bc6a7bd1a370e0761ad965cf8f3a3bfa3db55177
Joe Ferris <jferris@Shiny.local>
2008-04-24 19:52:56 -0400

    Converted assertions to matchers for test_labelled_form_helper

--- test/labelled_form/test_form_section_helper.rb
+++ test/labelled_form/test_form_section_helper.rb
@@ -21,40 +21,23 @@ describe "form section helpers" do
         <% end %>
         <%= s.output %>
       end_template
-      render(template)
+      @result = render(template)
+    end
+
+    it "should create a section_info div" do
+      @result.should have_tag('div[@class = "section_info"]')
     end
 		
     it "should create a title section" do
-      assert_tag({
-        :tag => 'div',
-        :attributes => {:class => 'section_info'},
-        :child => {
-          :tag => 'h2',
-          :content => 'Section Title'
-        }
-      })
+      @result.should have_text('//h2', 'Section Title')
     end
 
     it "should create an info section" do
-      assert_tag({
-        :tag => 'div',
-        :attributes => {:class => 'section_info'},
-        :child => {
-          :tag => 'p',
-          :content => 'I am the info'
-        }
-      })
+      @result.should have_text('.section_info p', 'I am the info')
     end
 
     it "should create a body section"do
-      assert_tag({
-        :tag => 'div',
-        :attributes => {:class => 'section_body'},
-        :child => {
-          :tag => 'p',
-          :content => 'I am the body'
-        }
-      })
+      @result.should have_text('.section_body p', 'I am the body')
     end
 
 	end
@@ -72,15 +55,11 @@ describe "form section helpers" do
           <% end %>
         <% end %>
       end_template
-      render(template)
+      @result = render(template)
     end
       
     it "should create a div for the section" do
-      assert_tag({
-        :tag => 'div',
-        :attributes => {:class => 'section'},
-        :child => {:tag => 'div'}
-      })
+      @result.should have_tag('//div[@class = "section"]')
     end
 
   end
@@ -98,96 +77,12 @@ describe "form section helpers" do
           <% end %>
         <% end %>
       end_template
-      render(template, :name => 'test')
+      @result = render(template, :name => 'test')
     end
 		
     it "should create the section body" do
-      assert_tag({
-        :tag => 'div',
-        :attributes => {:class => 'section_body'},
-        :child => {
-          :tag => 'input',
-          :attributes => {
-            :id => 'var_name',
-            :value => 'test'
-          }
-        }
-      })
-    end
-
-	end
-	
-  context "building a form section from a builder" do
-
-    setup do
-      template = <<-end_template
-        <% form_for(:var, @var) do |f| %>
-          <% f.section :title => 'Section Title' do |s| %>
-            <% s.info do %>
-              <p>I am the info</p>
-            <% end %>
-            <% s.body do %>
-              <%= s.text_field :name %>
-            <% end %>
-          <% end %>
-        <% end %>
-      end_template
-      render(template, :name => 'test')
-    end
-		
-    it "should create the form tag" do
-      assert_tag({
-        :tag => 'form',
-        :child => {
-          :tag => 'div',
-          :attributes => {:class => 'section'},
-          :child => {
-            :tag => 'div',
-            :attributes => {:class => 'section_body'},
-            :child => {
-              :tag => 'input',
-              :attributes => {
-                :id => 'var_name',
-                :value => 'test'
-              }
-            }
-          }
-        }
-      })
-    end
-
-	end
-	
-  context "building a section from a labelled from builder" do
-
-    before do
-      template = <<-end_template
-        <% labelled_form_for(:var, @var) do |f| %>
-          <% f.section :title => 'Section Title' do |s| %>
-            <% s.info do %>
-              <p>I am the info</p>
-            <% end %>
-            <% s.body do %>
-              <%= s.text_field :name %>
-            <% end %>
-          <% end %>
-        <% end %>
-      end_template
-      render(template, :name => 'test')
-    end
-
-    it "should build a labelled form" do
-      assert_tag({
-        :tag => 'form',
-        :attributes => {:class => 'labelled'},
-        :descendant => {
-          :tag => 'div',
-          :attributes => {
-            :class => 'value_field field',
-            :id => 'var_name_field'
-          }
-        }
-      })
+      @result.should have_tag('//div[@class = "section_body"]')
+      @result.should have_tag('//div/input')
     end
 
 	end
